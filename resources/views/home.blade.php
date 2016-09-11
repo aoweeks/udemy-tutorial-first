f434e708712461187ea079f260c859be2d752c16
Adam Weeks <aoweeks@gmail.com>
2016-09-11 16:37:17 +0000

    Add validation to nice action post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,15 @@
         <a href="{{ route('niceaction', ['action' => 'kiss']) }}">Kiss</a>
         <br>
         <br>
+        @if(count($errors) > 0)
+            <div>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <form action="{{ route('benice') }}" method="POST">
             {{ csrf_field() }}
             <label for="select-action">I want to...</label>
