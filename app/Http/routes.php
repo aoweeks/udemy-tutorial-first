234bbc1e771980446c5f1eace4281a8d40ac7776
Adam Weeks <aoweeks@gmail.com>
2016-09-11 12:13:11 +0000

    Add nice action routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('home');
 });
+
+
+Route::get('/greet', function () {
+    return view('actions.greet');
+})->name('greet');
+
+
+Route::get('/hug', function () {
+    return view('actions.hug');
+})->name('hug');
+
+
+Route::get('/kiss', function () {
+    return view('actions.kiss');
+})->name('kiss');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,8 @@
 
 @section('content')
     <div class="centered">
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, unde velit labore minus ut at aut odit consequuntur deserunt incidunt omnis nostrum mollitia autem quaerat corporis impedit distinctio cumque dolores!
-        </p>
-        <ul>
-            @for($i = 0; $i < 7; $i++)
-                @if($i % 2 === 0)
-                    <li>Iteration {{ $i + 1 }}</li>
-                @endif
-            @endfor
-        </ul>
+        <a href="{{ route('greet') }}">Greet</a>
+        <a href="{{ route('hug') }}">Hug</a>
+        <a href="{{ route('kiss') }}">Kiss</a>
     </div>
 @endsection
