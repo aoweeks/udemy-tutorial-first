ceeb6f9bd60df98eb74992459b6d814e7a11d643
Adam Weeks <aoweeks@gmail.com>
2016-09-14 17:40:56 +0000

    Reset to start new phase of tutorial

--- database/migrations/2016_09_14_161034_create_authors_table.php
+++ database/migrations/2016_09_14_161034_create_authors_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNiceActionsTable extends Migration
+class CreateAuthorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,10 @@ class CreateNiceActionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('nice_actions', function (Blueprint $table) {
+        Schema::create('authors', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('author');
             $table->timestamps();
-            $table->string('name')->unique();
-            $table->integer('niceness');
         });
     }
 
@@ -27,6 +26,6 @@ class CreateNiceActionsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('nice_actions');
+        Schema::drop('authors');
     }
 }
--- resources/views/actions/greet.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.master')
-
-@section('content')
-    <a href="{{ route('home') }}"><- Back</a>
-    <h1>I greet {{$name}}!</h1>
-@endsection
