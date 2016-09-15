e26012da15f7483264acb9670503373f5f7b1b7c
Adam Weeks <aoweeks@gmail.com>
2016-09-15 02:17:45 +0000

    Build index view skeleton

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,24 +11,6 @@
 |
 */
 
-Route::get('/', [
-    'uses' => 'NiceActionController@getHome',
-    'as' => 'home'
-]);
-
-
-
-Route::group(['prefix' => 'do'], function() {
-
-    Route::get('/{action}/{name?}', [
-        'uses' => 'NiceActionController@getNiceAction',
-        'as' => 'niceaction'
-    ]);
-    
-    
-    
-    Route::post('/add_action', [
-        'uses' => 'NiceActionController@postInsertNiceAction',
-        'as' => 'add_action'
-    ]);
+Route::get('/', function(){
+    return view('index');
 });
